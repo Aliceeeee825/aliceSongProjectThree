7a39869e9dbd1423d298ffd5f089bee788f8fa74
Aliceeeee825 <zhuyingsong0825@gmail.com>
2020-02-09 21:57:54 -0500

    basic logic and correct/incorrect effects added

--- script.js
+++ script.js
@@ -10,21 +10,28 @@ $(document).ready(function () {
     const directionGenerator = function(){
         let numDirection = randomArrow()
         if (numDirection == 1){
-            return direction = '.left'
+            return direction = 'left'
         }
 
         if (numDirection == 2) {
-            return direction = '.up'
+            return direction = 'up'
         }
 
         if (numDirection == 3) {
-            return direction = '.right'
+            return direction = 'right'
         }
 
         if (numDirection == 4) {
-            return direction = '.down'
+            return direction = 'down'
         }
     };
+
+    // function sleep(ms) {
+    //     return new Promise(resolve => setTimeout(resolve, ms));
+    // }
+
+    direction = directionGenerator()
+    
     
     // play button disappear after clicked
     $('button').on('click', function (e) {
@@ -32,12 +39,107 @@ $(document).ready(function () {
         $('.playButton').hide();
         $('.description').hide();
         $('h4').removeClass('invisible');
-        let direction = directionGenerator()
-        $(`${direction}`).removeClass('invisible');
+        $(`.${direction}`).removeClass('invisible');
     });
 
+    //check input 
+    // 37 = left 38 = up 39 = right 40 = down
+    //if direction left 
+                    //green
+                    //new direction
+                    //flag true
+                //else 
+                    //red
+                    //new direction
+                    //flag true 
+    // while (true){
+        let userInput = '';
+        document.addEventListener('keydown', function (event) {
+            if (event.keyCode === 37) {
+                if (direction === 'right' ){
+                    $(`.${direction}`).addClass('correct');
+                    setTimeout(() => {
+                        $(`.${direction}`).removeClass('correct');
+                        $(`.${direction}`).addClass('invisible');
+                        direction = directionGenerator();
+                        $(`.${direction}`).removeClass('invisible')
+                    }, 500)
+                }
+                
+                else{
+                    $(`.${direction}`).addClass('incorrect');
+                    setTimeout(() => {
+                        $(`.${direction}`).removeClass('incorrect');
+                        $(`.${direction}`).addClass('invisible');
+                        direction = directionGenerator();
+                        $(`.${direction}`).removeClass('invisible')
+                    }, 500)
+                }
+            }
 
+            else if (event.keyCode == 39) {
+                if (direction === 'left') {
+                    $(`.${direction}`).addClass('correct');
+                    setTimeout(() => {
+                        $(`.${direction}`).removeClass('correct');
+                        $(`.${direction}`).addClass('invisible');
+                        direction = directionGenerator();
+                        $(`.${direction}`).removeClass('invisible')}, 500)
+                }
 
+                else {
+                    $(`.${direction}`).addClass('incorrect');
+                    setTimeout(() => {
+                        $(`.${direction}`).removeClass('incorrect');
+                        $(`.${direction}`).addClass('invisible');
+                        direction = directionGenerator();
+                        $(`.${direction}`).removeClass('invisible')
+                    }, 500)
+                }
+            }
+            else if (event.keyCode == 38) {
+                if (direction === 'down') {
+                    $(`.${direction}`).addClass('correct');
+                    setTimeout(() => {
+                        $(`.${direction}`).removeClass('correct');
+                        $(`.${direction}`).addClass('invisible');
+                        direction = directionGenerator();
+                        $(`.${direction}`).removeClass('invisible')}, 500)
+                }
+
+                else {
+                    $(`.${direction}`).addClass('incorrect');
+                    setTimeout(() => {
+                        $(`.${direction}`).removeClass('incorrect');
+                        $(`.${direction}`).addClass('invisible');
+                        direction = directionGenerator();
+                        $(`.${direction}`).removeClass('invisible')
+                    }, 500)
+                }
+            }
+            else if (event.keyCode == 40) {
+                if (direction === 'up') {
+                    $(`.${direction}`).addClass('correct');
+                    setTimeout(() => {
+                        $(`.${direction}`).removeClass('correct');
+                        $(`.${direction}`).addClass('invisible');
+                        direction = directionGenerator();
+                        $(`.${direction}`).removeClass('invisible')}, 500)
+                }
+
+                else {
+                    $(`.${direction}`).addClass('incorrect');
+                    setTimeout(() => {
+                        $(`.${direction}`).removeClass('incorrect');
+                        $(`.${direction}`).addClass('invisible');
+                        direction = directionGenerator();
+                        $(`.${direction}`).removeClass('invisible')
+                    }, 500)
+                }
+            }
+            //else
+        });
+    // }
 });
 
 //console.log('hi')
