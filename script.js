a38378d226a2b8d7a68a237c49ebbdfe29c4d38f
Aliceeeee825 <zhuyingsong0825@gmail.com>
2020-02-13 10:34:11 -0500

    responsive design added, gesture logic for level two fixed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     let isLevelTwo = false;
     //Strech goal: Timer
-    const timerMax = 3;
+    const timerMax = 30;
     let timer = timerMax;
     $('.timeKeeper').html(`${timer}`);
     
@@ -13,6 +13,7 @@ $(document).ready(function () {
                 clearInterval(countdown)
                 $('.'+ level).removeClass('invisible');
                 $(`.${direction}`).addClass('invisible');
+                $('.socialMedia').addClass('socialMediaRelocation')
             }
         }, 1000)
     }
@@ -48,7 +49,7 @@ $(document).ready(function () {
     direction = directionGenerator()
     
     
-    // play button disappear after clicked
+    // click the play button
     $('.playButton').on('click', function (e) {
         e.preventDefault();
         $('.playButton').hide();
@@ -66,42 +67,63 @@ $(document).ready(function () {
         $(`.${direction}`).removeClass('invisible');
     }
 
+    function determination(arrowDirection){
+            if (isLevelTwo == false) {
+                //for level one
+                if (direction === `${arrowDirection}`) {
+                    answer = true
+                }
+                else {
+                    answer = false
+                }
+            }
+            
+            else{
+                //for level two
+                if (numOfCross % 2 === 0){
+                    //for even number of arrows
+                    if (direction === `${arrowDirection}`) {
+                        answer = true
+                    }
+                    else {
+                        answer = false
+                    }
+                }
+                
+                else if (numOfCross % 2 != 0) {
+                    if (direction === `${arrowDirection}`) {
+                        answer = false
+                    }
+                    else {
+                        answer = true
+                    }
+                }
+            }
+        }
+
     //check input 
     let answer = null
     let score = 0;
     document.addEventListener('keydown', function (event) {
         let userInput = '';
         if (event.keyCode === 37) {
-            if (direction === 'right' ){
-                answer = true
-            }
-            else{
-                answer = false
-            }
-        }
+            //press left 
+            determination('right')
+        } //press left ends
+
+        //press right
         else if (event.keyCode == 39) {
-            if (direction === 'left') {
-                answer = true
-            }
-            else {
-                answer = false
-            }
-        }
+            determination('left')
+        } //pree right ends
+
+        //press up
         else if (event.keyCode == 38) {
-            if (direction === 'down') {
-                answer = true
-            }
-            else {
-                answer = false
-            }
-        }
+            determination('down')
+        } //press up ends
+
+        //press down
         else if (event.keyCode == 40) {
-            if (direction === 'up') {
-                answer = true
-            }
-            else {
-                answer = false
-            }
+            determination('up')
         }
             
         if(answer !== null){
@@ -126,7 +148,6 @@ $(document).ready(function () {
         }
     });
 
-
     //restart button
     $('.restart').on('click', function(){
         window.location.reload(false);
@@ -150,8 +171,7 @@ $(document).ready(function () {
     }, false);
 
     function handleGesture() {
-        console.log("current dir"+direction)
-        let result = null
+        // let answer = null
         let up = touchstartY- touchendY
         let right = touchendX - touchstartX
         let y = Math.abs(up)
@@ -160,24 +180,14 @@ $(document).ready(function () {
             // swipe in up dir
             if (up >= 0){
                 // swipe up
-                console.log("swipe up")
-                if (direction === 'down'){
-                    result = true;
-                }
-                else{
-                    result = false;
-                }
+                determination('down');
+                console.log('up')
             }
 
             else{
-                console.log("swipe down")
                 //swipe down
-                if (direction === 'up'){
-                    result = true
-                }
-                else{
-                    result = false
-                }
+                determination('up')
+                console.log('down')
             }
         }
 
@@ -185,26 +195,19 @@ $(document).ready(function () {
             //swipe in left right directions
             if (right >= 0){
                 //swipe right
-                if (direction === 'left'){
-                    result = true
-                }
-                else{
-                    result = false
-                }
+                determination('left')
+                console.log('left')
             }
             else{
                 //swipe left 
-                if (direction === 'right'){
-                    result = true
-                }
-                else{
-                    result = false
-                }
+                determination('right')
+                console.log('right')
             }
         }
+        console.log(answer)
 
-        if (result !== null) {
-            if (result === true) {
+        if (answer !== null) {
+            if (answer === true) {
                 $(`.${direction}`).addClass('correct');
                 score++
                 $('.scoreKeeper').html(`${score}`)
@@ -235,68 +238,26 @@ $(document).ready(function () {
         $('.scoreKeeper').html(`${score}`);
         //timer
         timer = timerMax;
-        countdownFunction('levelThree');
+        countdownFunction('endingScreen');
     })
 
     let numOfCross = 0;
     function generateCross(){
         if (isLevelTwo){
             const modifyCross = Math.floor(Math.random() * 3) - 1;
-            const top = Math.random() * window.innerHeight * 0.8 + 0.2 * window.innerHeight;
-            const left = Math.random() * window.innerWidth
-            console.log(modifyCross)
+            const top = Math.random() * window.innerHeight * 0.6 + 0.2 * window.innerHeight;
+            const left = Math.random() * window.innerWidth * 0.8
             if (modifyCross > 0){
                 //add cross
                 $('ul').append(`<li class = "floatingCross"><i class="fas fa-times"></i></li>`)
                 $("li").last().css({top: top, left: left})
                 numOfCross ++
-                console.log(numOfCross)
             }else if (modifyCross < 0 && numOfCross > 0){
                 //remove cross
                 $('li').last().remove();
                 numOfCross --
-                console.log(numOfCross)
             }
         }
     }
-
-    // how many cross 
-    // if cross === 0, cannot substract
-    // remove: take one from list
-        //$('li').first().remove()
-    // add: add one to the list 
-        //ul append li.added${'.number'}
-        //$("li").last().css({top = top, left = left})
-    // do nothing
-
-
-
-    
-    // if (numberOfCross === 1){
-    //     const top = Math.random() * window.innerHeight * 0.8 + 0.2 * window.innerHeight;
-    //     const left = Math.random() * window.innerWidth
-    //     $('ul').append(`<li class = "floatingCross"><i class="fas fa-times"></i></li>`)
-
-    // }
-
-
-
-    //level three
-    $('.levelThree').on('click', function (e) {
-        e.preventDefault(e);
-        $('.levelThree').addClass('invisible')
-        $('.timeKeeper').html(`${timerMax}`);
-        $(`.${direction}`).removeClass('invisible');
-        score = 0
-        $('.scoreKeeper').html(`${score}`);
-        //timer
-        timer = timerMax;
-        countdownFunction('endingScreen');
-    })
-
-
-
-
-
 
 }); // the end of the document ready
